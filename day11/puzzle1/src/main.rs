dcc6e9ad7db9517d640d13c76b509cd02fbd4611
Markus.Weinand@kv-rlp.de <Markus.Weinand@kv-rlp.de>
2024-12-11 13:54:45 +0100

    more warnings removed

--- day11/puzzle1/src/main.rs
+++ day11/puzzle1/src/main.rs
@@ -4,10 +4,9 @@ fn main() {
     // let mut foo:Vec<i64> = vec![5];
     input.append(&mut foo);
     // let mut input:Vec<i64> = vec![125, 17];
-    for p in 0..75 {
+    for _ in 0..25 {
         let mut i = 0;
         let org_len = input.len();
-        let mut last_len = org_len;
         while i < org_len {
             let length = len_of_number(input[i]);
             if input[i] == 0 {
@@ -16,10 +15,6 @@ fn main() {
                 let x = (10 as i64).pow((length / 2) as u32) ;
                 let first = input[i] / x;
                 let second = input[i] % x;
-                // input.splice(i..i+1, vec![first, second]);
-                // i += 1;
-                // let first = input[i].to_string()[..length/2].parse::<i64>().unwrap();
-                // let second = input[i].to_string()[length/2..].parse::<i64>().unwrap();
                 input[i] = first;
                 input.push(second);
             }
@@ -28,9 +23,6 @@ fn main() {
             }
             i += 1;
         }
-        println!("{p}, {}, {}, {}", input.len(), input.len() - last_len, input.len() as f64 / last_len as f64);
-        last_len = input.len();
-        // println!("{}", input.len());
     }
     println!("{}", input.len());
 }
